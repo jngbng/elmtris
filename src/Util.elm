2f5e1332672349e1840d7a0cbe72ddb6ca7ad7cf
SUZUKI Shinichiro <shinichiro.su@gmail.com>
2015-04-03 11:54:48 +0900

    Update Util.elm

--- src/Util.elm
+++ src/Util.elm
@@ -1,9 +1,11 @@
 module Util where
 
+import List ((::))
+
 -- Creates an infinite list of the specified element
-repeat : a -> [a]
+repeat : a -> List a
 repeat a = a::(repeat a)
 
-replicate : Int -> a -> [a]
+replicate : Int -> a -> List a
 replicate n a =
   if n <= 0 then [] else a::(replicate (n-1) a)
